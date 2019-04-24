41996a4c225c13da79a1caede98a02e79642b367
Tatiana Slonimskaia <slonimskaia@gmail.com>
2019-04-24 23:54:15 +0200

    Create model classes for all the users

---
+++ src/main/java/com/codecool/quest_store/model/Codecooler.java
@@ -0,0 +1,27 @@
+package com.codecool.quest_store.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Codecooler extends User {
+
+    private List<Item> quests;
+    private List<Item> artifacts;
+    private int coins;//?
+    private int level;//?
+
+
+    public Codecooler(Builder builder) {
+        super(builder);
+        quests = new ArrayList<>();
+        artifacts = new ArrayList<>();
+    }
+
+    public List<Item> getQuests() {
+        return quests;
+    }
+
+    public List<Item> getArtifacts() {
+        return artifacts;
+    }
+}
