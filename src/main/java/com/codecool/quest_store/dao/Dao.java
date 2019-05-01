d8cb17a353edaaed76d16576ac0946fc8f93dc8e
mszotek <mszotek@gmail.com>
2019-05-01 11:08:43 +0200

    Implement generic DAO

--- src/main/java/com/codecool/quest_store/dao/Dao.java
+++ src/main/java/com/codecool/quest_store/dao/Dao.java
@@ -1,6 +1,6 @@
 package com.codecool.quest_store.dao;
 
-public interface DAO<T> {
-    void create(T item) throws DaoException;
-    void update(T item) throws DaoException;
+public interface Dao<T> {
+    void create(T thing) throws DaoException;
+    void update(T thing) throws DaoException;
 }
