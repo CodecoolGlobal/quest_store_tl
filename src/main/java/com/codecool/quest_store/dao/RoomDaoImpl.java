1cbf2c0b9b2dbe075b0b411819bd840cb28c3877
mszotek <mszotek@gmail.com>
2019-05-01 11:10:34 +0200

    Refactor RoomDAO for generic DAO

--- src/main/java/com/codecool/quest_store/dao/RoomDaoImpl.java
+++ src/main/java/com/codecool/quest_store/dao/RoomDaoImpl.java
@@ -5,21 +5,39 @@ import com.codecool.quest_store.model.Room;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class RoomDaoImpl implements RoomDao {
+public class RoomDaoImpl implements Dao<Room> {
 
     @Override
-    public void createRoom(Room room) throws DaoException {
-
+    public void create(Room thing) throws DaoException {
         String SQL = "INSERT INTO rooms (name) VALUES (?)";
 
         try (Connection connection = DatabaseConnector.getConnection();
              PreparedStatement pstmt = connection.prepareStatement(SQL)) {
-            pstmt.setString(1, room.getRoomName());
-            System.out.println("added " + room.getRoomName() + " to rooms");
+            pstmt.setString(1, thing.getRoomName());
+            System.out.println("added " + thing.getRoomName() + " to rooms");
         } catch (SQLException e){
-            throw new DaoException("failed to create room" + room.getRoomName(), e);
+            throw new DaoException("failed to create room " + thing.getRoomName(), e);
         }
 
+    }
+
+    @Override
+    public void update(Room thing) throws DaoException {
+
+        int id = thing.getId();
+        String newName = thing.getRoomName();
+
+        String SQL =
+                "UPDATE rooms SET name = " + newName
+                + " WHERE id = " + id;
+
+        try (Connection connection = DatabaseConnector.getConnection();
+             Statement stmt = connection.createStatement()){
+            stmt.executeUpdate(SQL);
+        } catch (SQLException e){
+            throw new DaoException("failed to update room " + newName, e);
+        }
     }
 }
