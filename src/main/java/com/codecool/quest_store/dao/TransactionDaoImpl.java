ecaf8fb31b5cd7056126fa58e04a20867170747e
Liz <pachaffy@gmail.com>
2019-05-04 02:43:31 +0200

    Refactor Transaction Dao Implementation
    - TransactionDaoImpl now implements generic Dao;
    - Remove String query from field area
    - Implement methods from Dao interface
    - Methods from Transaction Dao interface now take transaction as their parameter

--- src/main/java/com/codecool/quest_store/dao/TransactionDaoImpl.java
+++ src/main/java/com/codecool/quest_store/dao/TransactionDaoImpl.java
@@ -1,6 +1,7 @@
 package com.codecool.quest_store.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -9,12 +10,10 @@ import java.time.LocalDate;
 
 import com.codecool.quest_store.model.Transaction;
 
-public class TransactionDaoImpl implements TransactionDao {
-
-    private String query;
+public class TransactionDaoImpl implements TransactionDao, Dao<Transaction> {
 
     @Override
-    public void createTransaction(Transaction transaction) throws DaoException {
+    public void create(Transaction transaction) throws DaoException {
         String query = "INSERT INTO transactions (funding_id, user_id, timestamp, paid_amount) "
                 + "VALUES (?, ?, ?, ?)";
 
@@ -32,33 +31,62 @@ public class TransactionDaoImpl implements TransactionDao {
         }
     }
 
-    /**
-     *
-     * @param column (id_user or id_team)
-     * @param columnId (id of user or id of team)
-     */
-
     @Override
-    public void updateTransaction(Transaction transaction) throws DaoException {
+    public void update(Transaction transaction) throws DaoException {
+        String query = "UPDATE transactions "
+                + "SET funding_id = ?, "
+                + "user_id = ?, "
+                + "timestamp = ?, "
+                + "paid_amount = ? "
+                + "WHERE id = ?";
 
-//        String query = "UPDATE transactions "
-//                + "SET funding_id = ? "; //Update the whole transaction (multiple sets)
-//
-//        try (Connection connection = DatabaseConnector.getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-//
-//            preparedStatement.setInt(1, status);
-//            preparedStatement.setInt(2, columnId);
-//            preparedStatement.setInt(3, idFunding);
-//            preparedStatement.executeUpdate();
-//
-//        } catch (SQLException e) {
-//            throw new DaoException("Failed to update the transaction's status\n" + e);
-//        }
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, transaction.getFUNDING_ID());
+            preparedStatement.setInt(2, transaction.getUSER_ID());
+            preparedStatement.setDate(3, Date.valueOf(transaction.getTIMESTAMP()));
+            preparedStatement.setInt(4, transaction.getPAID_AMOUNT());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DaoException("Failed to update the transaction's status\n" + e);
+        }
     }
 
     @Override
-    public int getPriceSumOfRealizedQuests(int userId) throws DaoException {
+    public Transaction extractFromResultSet(ResultSet resultSet) throws DaoException {
+        int ID;
+        int FUNDING_ID;
+        int USER_ID;
+        LocalDate TIMESTAMP;
+        int PAID_AMOUNT;
+        Transaction transaction;
+
+        try {
+            ID = resultSet.getInt("id");
+            FUNDING_ID = resultSet.getInt("funding_id");
+            USER_ID = resultSet.getInt("user_id");
+            TIMESTAMP = resultSet.getDate("timestamp").toLocalDate();
+            PAID_AMOUNT = resultSet.getInt("paid_amount");
+        } catch (SQLException e) {
+            throw new DaoException("Failed to get transaction from result set\n" + e);
+        }
+
+        transaction = new Transaction.Builder()
+                .withID(ID)
+                .withFUNDING_ID(FUNDING_ID)
+                .withUSER_ID(USER_ID)
+                .withTIMESTAMP(TIMESTAMP)
+                .withPAID_AMOUNT(PAID_AMOUNT)
+                .build();
+
+        return transaction;
+    }
+
+    @Override
+    public int getPriceSumOfRealizedQuests(Transaction transaction) throws DaoException {
+        int userId = transaction.getUSER_ID();
         String query = "SELECT SUM(price) FROM items "
                 + "INNER JOIN transactions "
                 + "ON transactions.id_item = items.id "
@@ -68,12 +96,12 @@ public class TransactionDaoImpl implements TransactionDao {
                 + "ON transactions.id_status = statuses.id "
                 + "WHERE transactions.id_user = ? "
                 + "AND item_types.type='Quest' AND statuses.type='Realized'";
-
         return getSumOfPrices(userId, query);
     }
 
     @Override
-    public int getPriceSumOfPurchasedArtifacts(int userId) throws DaoException {
+    public int getPriceSumOfPurchasedArtifacts(Transaction transaction) throws DaoException {
+        int userId = transaction.getUSER_ID();
         String query = "SELECT SUM(price) FROM items "
                 + "INNER JOIN transactions "
                 + "ON transactions.id_item = items.id "
@@ -83,8 +111,7 @@ public class TransactionDaoImpl implements TransactionDao {
                 + "ON transactions.id_status = statuses.id "
                 + "WHERE transactions.id_user = ? "
                 + "AND item_types.type='Artifact' "
-                + "AND (statuses.type='Realized' OR statuses.type='Pending')";
-
+                + "AND (statuses.type='Realized' OR statuses.type='Pending' OR statuses.type='In progress')";
         return getSumOfPrices(userId, query);
     }
 
