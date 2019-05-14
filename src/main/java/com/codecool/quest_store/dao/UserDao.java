9c1cec3148dc85d956fdfa2e95d47ff83acd6b00
Tatiana Slonimskaia <slonimskaia@gmail.com>
2019-05-14 14:30:40 +0200

    Refactor: use one dao for each daoimpl classes

--- src/main/java/com/codecool/quest_store/dao/UserDao.java
+++ src/main/java/com/codecool/quest_store/dao/UserDao.java
@@ -6,7 +6,4 @@ import java.util.List;
 
 public interface UserDao {
     List<User> getUsersByType(int userType) throws DaoException;
-    void createUser(User user) throws DaoException;
-    void updateUserEmail(User user, String email) throws DaoException;
-    void updateUserRoom(User user, int room) throws DaoException;
 }
