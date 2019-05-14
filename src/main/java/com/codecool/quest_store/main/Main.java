b7647972f771af752648f1cd8ea9d56d37eb1562
mszotek <mszotek@gmail.com>
2019-05-14 15:26:37 +0200

    Display normal and magic artifacts

--- src/main/java/com/codecool/quest_store/main/Main.java
+++ src/main/java/com/codecool/quest_store/main/Main.java
@@ -1,5 +1,6 @@
 package com.codecool.quest_store.main;
 
+import com.codecool.quest_store.controllers.ArtifactsController;
 import com.codecool.quest_store.controllers.LoginController;
 import com.codecool.quest_store.controllers.Static;
 import com.codecool.quest_store.utility.FlywayMigration;
@@ -9,12 +10,13 @@ import java.net.InetSocketAddress;
 
 public class Main {
     public static void main( String[] args ) throws Exception {
-        //FlywayMigration.migrateDatabase();
-        ////http://localhost:8000/login
+//        FlywayMigration.migrateDatabase();
+//        http://localhost:8000/login
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         server.createContext("/static", new Static());
         server.createContext("/login", new LoginController());
+        server.createContext("/artifacts", new ArtifactsController());
         server.setExecutor(null);
 
         server.start();
