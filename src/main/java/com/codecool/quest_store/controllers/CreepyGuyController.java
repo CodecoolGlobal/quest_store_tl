c2a981199ac2ee5b20c735cf10ee9e7f2b151f35
Liz <pachaffy@gmail.com>
2019-05-16 00:47:07 +0200

    Implement method that handles POST

--- src/main/java/com/codecool/quest_store/controllers/CreepyGuyController.java
+++ src/main/java/com/codecool/quest_store/controllers/CreepyGuyController.java
@@ -1,21 +1,19 @@
 package com.codecool.quest_store.controllers;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 
 import java.nio.charset.StandardCharsets;
 
-import java.net.URLDecoder;
-
-import com.codecool.quest_store.service.CreepyGuyService;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
+
+import com.codecool.quest_store.service.ServiceUtility;
+import com.codecool.quest_store.service.CreepyGuyService;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -23,9 +21,11 @@ import com.sun.net.httpserver.HttpHandler;
 public class CreepyGuyController implements HttpHandler {
 
     private CreepyGuyService creepyGuyService;
+    private ServiceUtility serviceUtility;
 
     public CreepyGuyController() {
         creepyGuyService = new CreepyGuyService();
+        serviceUtility = new ServiceUtility();
     }
 
     @Override
@@ -46,9 +46,9 @@ public class CreepyGuyController implements HttpHandler {
             InputStreamReader inputStreamReader = new InputStreamReader(httpExchange.getRequestBody(), StandardCharsets.UTF_8);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
             String data = bufferedReader.readLine();
-            Map<String, String> inputs = parse(data);
+            Map<String, String> inputs = serviceUtility.parseData(data, ServiceUtility.AND);
 
-            handlePOST(inputs);
+            handlePOST(inputs, model);
 
             response = template.render(model);
             httpExchange.getResponseHeaders().set("Location", "/creepy-guy");
@@ -60,30 +60,32 @@ public class CreepyGuyController implements HttpHandler {
         outputStream.close();
     }
 
-    private Map<String, String> parse(String data) throws UnsupportedEncodingException {
+    private void handlePOST(Map<String, String> inputs, JtwigModel model) {
 
-        Map<String, String> map = new HashMap<>();
-        String[] pairs = data.split("&");
-        String[] keyValue;
-        String value;
-
-        for (String pair : pairs) {
-            keyValue = pair.split("=");
-            value = URLDecoder.decode(keyValue[1], "UTF-8");
-            map.put(keyValue[0], value);
-        }
-        return map;
-    }
-
-    private void handlePOST(Map<String, String> inputs) {
+        String name;
 
         for (Map.Entry<String, String> entry : inputs.entrySet()) {
+
             if (entry.getKey().contains("recruit-mentor-button")) {
-                String name = inputs.get("mentor-name");
+                name = inputs.get("mentor-name");
                 String surname = inputs.get("mentor-surname");
                 String email = inputs.get("mentor-email");
+
                 creepyGuyService.createMentor(name, surname, email);
+                showNotificationWithStatus(model, "Mentor has been recruited.");
+                //TODO: pause before redirect
+
+            } else if (entry.getKey().contains("create-room-button")) {
+                name = inputs.get("room-name");
+                creepyGuyService.createRoom(name);
+                showNotificationWithStatus(model, "Room has been created.");
+                //TODO: pause before redirect
             }
         }
+    }
+
+    private void showNotificationWithStatus(JtwigModel model, String notification) {
+        model.with("set_visibility", "visible");
+        model.with("message", notification);
     }
 }
