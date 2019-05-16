19a326d3a962d56650068cf8f6b86146f0eaab54
Liz <pachaffy@gmail.com>
2019-05-16 15:10:56 +0200

    Move functionality of creepy-guy to creepy-guy.js

--- src/main/resources/static/scripts/configuration-menu.js
+++ src/main/resources/static/scripts/configuration-menu.js
@@ -2,8 +2,6 @@
 
 window.onload = function () {
     EnableEventListenerToggleHideShowForm();
-    addEventListenerForViewMentorsButton();
-    addEventListenerForNotificationOfMentor();
 };
 
 function EnableEventListenerToggleHideShowForm() {
@@ -33,28 +31,4 @@ function toggleHideShowActionForm(actionForm) {
     } else {
         closeActionForm(actionForm);
     }
-}
-
-function showMentorNotification() {
-    let notificationContainer = document.getElementById("creepy-guy-notification-message");
-    notificationContainer.style.visibility = "visible";
-    notificationContainer.innerText = "Mentor has been recruited.";
-}
-
-function redirectToMentorsPage() {
-    window.location.href="/mentors";
-}
-
-function addEventListenerForViewMentorsButton() {
-    let viewMentorsButton = document.getElementById("view-mentors-button");
-    viewMentorsButton.addEventListener("click", redirectToMentorsPage);
-}
-
-function addEventListenerForNotificationOfMentor() {
-    let submitButton = document.getElementById("create-mentor-form");
-    submitButton.addEventListener("submit", handleMentorNotification);
-}
-
-function handleMentorNotification() {
-    showMentorNotification();
 }
