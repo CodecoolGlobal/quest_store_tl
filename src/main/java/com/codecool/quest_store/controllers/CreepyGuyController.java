f2b4ca204e55767ad0a5b4176184b246fe1f36e6
Liz <pachaffy@gmail.com>
2019-05-17 02:49:30 +0200

    Refactor Creepy Guy Controller, separate handle method into smaller methods

--- src/main/java/com/codecool/quest_store/controllers/CreepyGuyController.java
+++ src/main/java/com/codecool/quest_store/controllers/CreepyGuyController.java
@@ -2,11 +2,7 @@ package com.codecool.quest_store.controllers;
 
 import java.util.Map;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-
-import java.nio.charset.StandardCharsets;
 
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
@@ -38,47 +34,49 @@ public class CreepyGuyController implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/creepy-guy.twig");
-        JtwigModel model = JtwigModel.newModel();
-
         String method = httpExchange.getRequestMethod();
-        String response;
 
         if (method.equals("GET")) {
-            User student = userService.getUserByCookie(httpExchange.getRequestHeaders().get("Cookie").get(0));
-            response = template.render(model);
-            ServiceUtility.sendResponse(httpExchange, response);
+            User activeUser = userService.getUserByCookie(httpExchange.getRequestHeaders().get("Cookie").get(0));
+            renderCreepyGuy(httpExchange);
         }
 
         if (method.equals("POST")) {
-            InputStreamReader inputStreamReader = new InputStreamReader(httpExchange.getRequestBody(), StandardCharsets.UTF_8);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            String data = bufferedReader.readLine();
-            Map<String, String> inputs = ServiceUtility.parseData(data, ServiceUtility.AND);
-
-            handlePOST(inputs);
-
-            response = template.render(model);
-            ServiceUtility.redirectToContext(httpExchange, response, "/creepy-guy");
+            handlePOST(httpExchange);
         }
     }
 
-    private void handlePOST(Map<String, String> inputs) {
-
-        String name;
+    private void handlePOST(HttpExchange httpExchange) throws IOException {
+        Map<String, String> inputs = ServiceUtility.getPOSTInputs(httpExchange);
 
         for (Map.Entry<String, String> entry : inputs.entrySet()) {
 
             if (entry.getKey().contains("recruit-mentor-button")) {
-                name = inputs.get("mentor-name");
-                String surname = inputs.get("mentor-surname");
-                String email = inputs.get("mentor-email");
-                employeeService.createUser(name, surname, email, UserType.MENTOR, UserDefaultPhoto.MENTOR);
+                handleCreateMentor(inputs);
 
             } else if (entry.getKey().contains("create-room-button")) {
-                name = inputs.get("room-name");
-                creepyGuyService.createRoom(name);
+                handleCreateRoom(inputs);
             }
         }
+        ServiceUtility.redirectToContext(httpExchange, "", "/creepy-guy");
+    }
+
+    private void renderCreepyGuy(HttpExchange httpExchange) throws IOException {
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/creepy-guy.twig");
+        JtwigModel model = JtwigModel.newModel();
+        String response = template.render(model);
+        ServiceUtility.sendResponse(httpExchange, response);
+    }
+
+    private void handleCreateMentor(Map<String, String> inputs) {
+        String name = inputs.get("mentor-name");
+        String surname = inputs.get("mentor-surname");
+        String email = inputs.get("mentor-email");
+        employeeService.createUser(name, surname, email, UserType.MENTOR, UserDefaultPhoto.MENTOR);
+    }
+
+    private void handleCreateRoom(Map<String, String> inputs) {
+        String name = inputs.get("room-name");
+        creepyGuyService.createRoom(name);
     }
 }
