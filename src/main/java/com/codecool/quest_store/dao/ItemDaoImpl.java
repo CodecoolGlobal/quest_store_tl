c878ebcbe97946a5c4d9e52dc5891a4e0a39298b
Liz <pachaffy@gmail.com>
2019-06-02 23:36:20 +0200

    Implement getItemTypes method in ItemDaoImpl.java

--- src/main/java/com/codecool/quest_store/dao/ItemDaoImpl.java
+++ src/main/java/com/codecool/quest_store/dao/ItemDaoImpl.java
@@ -1,10 +1,13 @@
 package com.codecool.quest_store.dao;
 
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.List;
 
 import com.codecool.quest_store.model.Item;
 
@@ -83,35 +86,34 @@ public class ItemDaoImpl implements ItemDao {
         return item;
     }
 
-    public List<Item> getAllItems() throws DaoException{
+    public List<Item> getAllItems() throws DaoException {
         String query =
                 "SELECT * FROM items";
-//                "SELECT * FROM items WHERE item_type = 1 OR item_type = 2;";
         List<Item> allArtifacts;
 
         try (Connection connection = DatabaseConnector.getConnection();
-            PreparedStatement pstmt = connection.prepareStatement(query);
-            ResultSet rs = pstmt.executeQuery()){
-                allArtifacts = getListByResultSet(rs);
-        } catch (SQLException e){
+             PreparedStatement pstmt = connection.prepareStatement(query);
+             ResultSet rs = pstmt.executeQuery()) {
+            allArtifacts = getListByResultSet(rs);
+        } catch (SQLException e) {
             throw new DaoException("Failed to get all items\n", e);
         }
         return allArtifacts;
     }
 
-    public Item getItemById(int itemId) throws DaoException{
+    public Item getItemById(int itemId) throws DaoException {
         String query =
                 "SELECT * FROM items WHERE id=?;";
-        try(Connection connection = DatabaseConnector.getConnection();
-            PreparedStatement pstmt = connection.prepareStatement(query)){
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
             pstmt.setInt(1, itemId);
-            try{
+            try {
                 ResultSet rs = pstmt.executeQuery();
                 return getListByResultSet(rs).get(0);
-            } catch (SQLException e){
+            } catch (SQLException e) {
                 throw new DaoException("Failed to get item by id", e);
             }
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("Failed to get item by id", e);
         }
     }
@@ -121,12 +123,12 @@ public class ItemDaoImpl implements ItemDao {
         String query =
                 "SELECT discount FROM discount;";
 
-        try(Connection connection = DatabaseConnector.getConnection();
-            PreparedStatement pstmt = connection.prepareStatement(query);
-            ResultSet rs = pstmt.executeQuery()){
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query);
+             ResultSet rs = pstmt.executeQuery()) {
             rs.next();
             return rs.getInt("discount");
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("Failed to get discount", e);
         }
     }
@@ -136,31 +138,58 @@ public class ItemDaoImpl implements ItemDao {
         String query =
                 "UPDATE discount SET discount = ?;";
 
-        try(Connection connection = DatabaseConnector.getConnection();
-            PreparedStatement pstmt = connection.prepareStatement(query)){
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
             pstmt.setInt(1, newDiscount);
             pstmt.executeUpdate();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("Failed to update new discount", e);
         }
     }
 
-    public List<Item> getUserItems(int user_id) throws DaoException{
+    public List<Item> getUserItems(int user_id) throws DaoException {
         String query = "SELECT * FROM items " +
                 "INNER JOIN fundings ON items.id = fundings.item_id " +
                 "INNER JOIN transactions ON transactions.funding_id = fundings.id " +
                 "INNER JOIN status_history ON status_history.funding_id = fundings.id " +
                 "INNER JOIN statuses ON statuses.id = status_history.status_id " +
                 "WHERE transactions.user_id = ? AND statuses.type = 'Pending';";
-        try(Connection connection = DatabaseConnector.getConnection();
-            PreparedStatement pstmt = connection.prepareStatement(query)){
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
             pstmt.setInt(1, user_id);
             ResultSet rs = pstmt.executeQuery();
 
             return getListByResultSet(rs);
 
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw new DaoException("Failed to get item by id", e);
         }
+    }
+
+    @Override
+    public Map<String, Integer> getItemTypes() throws DaoException {
+        String query = "SELECT id, grade FROM item_types";
+        try (Connection connection = DatabaseConnector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            return getItemTypesFrom(preparedStatement);
+        } catch (SQLException e) {
+            throw new DaoException("Failed to get item types.");
+        }
+    }
+
+    private Map<String, Integer> getItemTypesFrom(PreparedStatement preparedStatement) throws DaoException {
+        Map<String, Integer> itemTypes = new HashMap<>();
+        Integer id;
+        String itemGrade;
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                id = resultSet.getInt("id");
+                itemGrade = resultSet.getString("grade");
+                itemTypes.put(itemGrade, id);
+            }
+        } catch (SQLException e) {
+            throw new DaoException("Failed to populate map of item types");
+        }
+        return itemTypes;
     }
 }
