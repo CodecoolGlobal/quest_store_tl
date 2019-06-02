330d5fa0770037b08117c54bf31fc72a98afc1b6
Liz <pachaffy@gmail.com>
2019-06-02 22:08:03 +0200

    Add getUderTypes() method in UserDao

--- src/main/java/com/codecool/quest_store/dao/UserDao.java
+++ src/main/java/com/codecool/quest_store/dao/UserDao.java
@@ -1,9 +1,13 @@
 package com.codecool.quest_store.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import com.codecool.quest_store.model.User;
 
-import java.util.List;
-
 public interface UserDao {
+
     List<User> getUsersByType(int userType) throws DaoException;
+
+    Map<String, Integer> getUserTypes() throws DaoException;
 }
