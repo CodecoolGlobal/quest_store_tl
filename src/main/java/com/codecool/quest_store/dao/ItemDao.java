75155494e7fecfbeb1bdce1d2047b4343c056ba3
Liz <pachaffy@gmail.com>
2019-06-02 23:35:13 +0200

    Add getItemTypes method in ItemDao interface

--- src/main/java/com/codecool/quest_store/dao/ItemDao.java
+++ src/main/java/com/codecool/quest_store/dao/ItemDao.java
@@ -1,16 +1,20 @@
 package com.codecool.quest_store.dao;
 
-import com.codecool.quest_store.model.Item;
-
 import java.util.List;
+import java.util.Map;
+
+import com.codecool.quest_store.model.Item;
 
 public interface ItemDao extends Dao<Item> {
 
     List<Item> getAllItems() throws DaoException;
+
     Item getItemById(int itemId) throws DaoException;
 
     int getDiscount() throws DaoException;
 
     void setDiscount(int newDiscount) throws DaoException;
+
+    Map<String, Integer> getItemTypes() throws DaoException;
 }
 
