ab32859afd0058e26440ab31ad8ecbfcce617df4
Liz <pachaffy@gmail.com>
2019-06-02 21:50:36 +0200

    Modify printError method

--- src/main/java/com/codecool/quest_store/view/View.java
+++ src/main/java/com/codecool/quest_store/view/View.java
@@ -2,19 +2,17 @@ package com.codecool.quest_store.view;
 
 public class View {
 
-    private final String RESET = "\u001B[0m";
-
     private void print(String message) {
         System.out.println(message);
     }
 
     public void printError(String message) {
-        final String RED = "\u001B[31m";
-        print(RED + message + RESET);
+        System.err.println(message);
     }
 
     public void printSuccess(String message) {
         final String GREEN = "\u001B[32m";
+        final String RESET = "\u001B[0m";
         print(GREEN + message + RESET);
     }
 }
